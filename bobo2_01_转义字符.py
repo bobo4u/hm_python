6d3ce1a42d77a5e90ef8d85e35c7ad201d8ff6d8
bobo <1053538078@qq.com>
2022-02-06 19:06:25 +0800

    0206

---
+++ bobo2_01_转义字符.py
@@ -0,0 +1,9 @@
+print("hello\nworld")
+"""
+\n
+\t
+\r 覆盖
+\b 退一个格
+"""
+
+print("http:\\\\www.baidu.com")
---
+++ bobo2_03_bool.py
@@ -0,0 +1,7 @@
+from html.entities import name2codepoint
+
+
+n1 = True
+n2 = False
+print(n1+1)
+print(n2+1)
