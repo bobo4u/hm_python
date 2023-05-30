c0212da2e739140220f747fba0f781e9a51eb6d4
bobo4u <1053538078@qq.com>
2023-05-30 13:25:49 +0800

    日常code练习

--- 日常练习/class_student.py
+++ 日常练习/class_student.py
@@ -9,10 +9,28 @@ class Student():
             grade = int(input("请输入你的成绩: "))
             self.grades.append(grade)
         return self.grades
+    def printGrades(self):
+        for i in range(0,self.nu):
+            print(self.grades[i])
+    def avGrades(self):
+        totle = 0 
+        for i in range(0,self.nu):
+            totle = totle + self.grades[i] 
+        ave = totle / self.nu   
 
-    
-
-s1 = Student('bobo','liu')
-a = s1.inputGrade(3)
-print(s1.first,s1.last)
-print(a)
+        return ave
+    def highLow(self):
+        high = 0
+        low = 100
+        for i in range(0,self.nu):
+            if self.grades[i] > high:
+                high = self.grades[i]
+            if self.grades[i] < low:
+                low = self.grades[i]
+        return high,low
+student1 = Student("liu","bobo")
+student1.inputGrade(4)
+print(student1.grades)
+ave1 = student1.avGrades()
+print(ave1)
+print(student1.highLow())
