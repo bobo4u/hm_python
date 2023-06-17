3c649617c43ce3dfc1ea486fc703a07b89a1237c
bobo4u <1053538078@qq.com>
2023-06-17 21:08:12 +0800

    尝试使用鼠标绘制目标框

--- opencv_roi.py
+++ opencv_roi.py
@@ -1,5 +1,17 @@
 import cv2
 print(cv2.__version__)
+
+def mouseClick(event,xPOS,yPOS,flags,params):
+    
+    
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pass
+    if event == cv2.EVENT_LBUTTONUP:
+        pass
+    if event == cv2.EVENT_RBUTTONUP:
+        pass
+
+
 width=640
 height=520
 cam=cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -7,6 +19,12 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
+cv2.namedWindow("my WEBcam")
+cv2.setMouseCallback("my WEBcam",mouseClick)
+
+
+
+
 while True:
     ignore,  frame = cam.read()
     cv2.imshow('my WEBcam', frame)
@@ -16,7 +34,7 @@ while True:
     cv2.imshow("my Gray",frameGray)
     cv2.moveWindow("my Gray",650,0)
 
-    frameBGR = frame[150:210,250:390]
+    frameBGR = frame[250:520,25:520]
     cv2.imshow('my frameBGR', frameBGR)
     row = 0
     colume = 0
